417d803f23a95c1ae54d604631e0344007c1ecbd
isadorabnunes <isadora.nunes@novaandradina.org>
2019-06-22 17:12:43 -0300

    Atualizando o Front

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,25 +1,79 @@
 import React, { Component } from 'react';
-import {StyleSheet} from 'react-native'
-import { Provider as PaperProvider, Appbar } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import {
+    Provider as PaperProvider,
+    Surface,
+    Button,
+    Title,
+} from 'react-native-paper';
 
 const styles = StyleSheet.create({
-    bottom: {
-      position: 'absolute',
-      left: 0,
-      right: 0,
-      bottom: 0,
+    PaperProvider: {
+        backgroundColor: '#87CEFA',
     },
-  })
+    title: {
+        justifyContent: 'center',
+        fontSize: 16,
+    },
+    surface: {
+        width: 150,
+        height: 150,
+        elevation: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
+    },
+    TeyxtInput: {
+        borderColor: '#4682B4',
+        color: "#FFFFFF",
+        }
+    }
+})
 
 class register extends Component {
     render() {
         return (
-            <Appbar>
-                <Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
-                <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
-                <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
-                <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
-            </Appbar>
+            <PaperProvider>
+                <Title style={styles.title}>CartNote</Title>
+
+                <Button style={styles.Loginbottom} mode="contained" onPress={() => }> Login </Button>
+                <Button style={styles.SignUpbottom} mode="contained" onPress={() => }> Sign Up </Button>
+
+                <Surface style={styles.surface}>
+                    <Surface style={styles.surfaceLogin}>
+                        <TextInput
+                            label='Email'
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text })}
+                        />
+                        <TextInput
+                            label='Senha'
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text })}
+                        />
+                        <Button style={styles.surfacebottomLogin} mode="contained" onPress={() => }> Login </Button>
+                    </Surface>
+
+                    <Surface style={styles.surfaceSignUp}>
+                        <TextInput
+                            label='Username'
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text })}
+                        />
+                        <TextInput
+                            label='Email'
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text })}
+                        />
+                        <TextInput
+                            label='Senha'
+                            value={this.state.text}
+                            onChangeText={text => this.setState({ text })}
+                        />
+                        <Button style={styles.surfaceBottomSignUp} mode="contained" onPress={() => }> Sign Up </Button>
+                    </Surface>
+                </Surface>
+            </PaperProvider>
         )
     }
 }
