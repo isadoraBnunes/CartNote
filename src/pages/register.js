1cc24a16a7e503b73fdb0b896472ea7d7524b873
isadorabnunes <isadora.nunes@novaandradina.org>
2019-06-21 22:23:49 -0300

    AtualizandoRegister

---
+++ src/pages/register.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {StyleSheet} from 'react-native'
+import { Provider as PaperProvider, Appbar } from 'react-native-paper';
+
+const styles = StyleSheet.create({
+    bottom: {
+      position: 'absolute',
+      left: 0,
+      right: 0,
+      bottom: 0,
+    },
+  })
+
+class register extends Component {
+    render() {
+        return (
+            <Appbar>
+                <Appbar.Action icon="archive" onPress={() => console.log('Pressed archive')} />
+                <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+                <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+                <Appbar.Action icon="delete" onPress={() => console.log('Pressed delete')} />
+            </Appbar>
+        )
+    }
+}
+
+export default register
